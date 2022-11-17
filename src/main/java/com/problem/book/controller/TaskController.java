2921a790366fd705a2c2084297e601035eb1c84b
Observer <kirillkuznetsov181@gmail.com>
2022-11-17 17:04:18 +0200

    adding tasks

--- src/main/java/com/problem/book/controller/TaskController.java
+++ src/main/java/com/problem/book/controller/TaskController.java
@@ -1,8 +1,13 @@
 package com.problem.book.controller;
 
+import com.problem.book.models.Tasks;
 import com.problem.book.repo.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TaskController {
@@ -10,5 +15,25 @@ public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
 
+    @GetMapping("/")
+    public String home(Model model) {
+        Iterable<Tasks> data = taskRepository.findAll();
+        model.addAttribute("tasks", data);
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
 
+    @PostMapping("/")
+    public String saveTask(@RequestParam String task, Model model) {
+        if (task.length() < 5) {
+            Iterable<Tasks> t = taskRepository.findAll();
+            model.addAttribute("tasks", t);
+            model.addAttribute("title", "Главная страница");
+            model.addAttribute("error", "Текст задания не менее 5 символов");
+            return "home";
+        }
+        Tasks taskObj = new Tasks(task);
+        taskRepository.save(taskObj);
+        return "redirect:/";
+    }
 }
--- src/main/java/com/problem/book/models/Tasks.java
+++ src/main/java/com/problem/book/models/Tasks.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -17,9 +14,13 @@ import javax.persistence.Table;
 public class Tasks {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     @Column( name = "task")
     private String task;
 
+    public Tasks(String task) {
+        this.task = task;
+    }
 }
